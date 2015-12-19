2471d6400fa08445f466583c67d33266aa03c9b3
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-19 09:14:44 +0900

    Add timer which can start but cannot pause, stop.

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var time = 0
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func play(sender: UIBarButtonItem) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+    }
 
+    @IBAction func pause(sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func reset(sender: UIBarButtonItem) {
+        
+    }
+    
+    func increaseTimer() {
+        time++
+        timerLabel.text = String(time)
+    }
 }
 
